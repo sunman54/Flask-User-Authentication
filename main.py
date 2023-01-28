887641092ff92b104b1ac66d94b5d1b949d23e8b
sunman54 <m.sunman@yandex.com>
2023-01-28 21:29:33 +0300

    database done !

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, url_for, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisisasecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.permanent_session_lifetime = timedelta(minutes=1) # days=1
 
+db = SQLAlchemy(app)
 
 
-
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column('id', db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    note = db.Column(db.String(400))
+    def __int__(self, name, note):
+        self.name = name
+        self.note = note
 
 @app.route('/')
 def home():
@@ -23,6 +33,15 @@ def login():
         session.permanent = True
         user = request.form['name']
         session['user'] = user
+
+        found_user = Users.query.filter_by(name = user).first()
+
+        if found_user:
+            session['note'] = found_user.note
+        else:
+            usr = Users(name = user, note = "")
+            db.session.add(usr)
+            db.session.commit()
         return redirect(url_for('user'))
 
     else:
@@ -31,10 +50,24 @@ def login():
         else:
             return render_template('login.html')
 
-@app.route('/user/')
+@app.route('/user/', methods=['GET', 'POST'])
 def user():
         if 'user' in session:
             user = session['user']
+
+            if request.method == 'POST':
+                note = request.form['note']
+                session['note'] = note
+                found_user = Users.query.filter_by(name=user).first()
+                found_user.note = note
+
+                db.session.commit()
+
+            else:
+                if 'note' in session:
+                    note = session['note']
+
+
             return render_template('dashboard.html', user = user)
         else:
             return redirect(url_for('login'))
@@ -44,5 +77,12 @@ def logout():
     session.pop('user', None )
     return redirect(url_for('home'))
 
+
+@app.route('/view')
+def view():
+    return render_template('view.html', values = Users.query.all())
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=5001)
